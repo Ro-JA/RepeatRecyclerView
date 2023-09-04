09d063fb031176993a5afe84d32511bbe5d637a7
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-09-05 07:42:02 +0900

    not work

--- app/src/main/java/com/example/repeatrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/repeatrecyclerview/MainActivity.kt
@@ -8,14 +8,28 @@ import com.example.repeatrecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var index = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val myDataSet = Datasource().loadMessages()
+      /*  val myDataSet = Datasource().loadMessages()
         binding.recyclerView.adapter = ItemAdapter(this, myDataSet)
-        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.setHasFixedSize(true)*/
 
+    }
+
+    private fun init(){
+        val myDatasource = Datasource().loadMessages()
+        binding.apply {
+            recyclerView.adapter = ItemAdapter(this@MainActivity, myDatasource)
+            recyclerView.setHasFixedSize(true)
+            button.setOnClickListener {
+                if (index > 9) index = 0
+                val messages = myDatasource[index]
+
+            }
+        }
     }
 }
--- app/src/main/java/com/example/repeatrecyclerview/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/repeatrecyclerview/adapter/ItemAdapter.kt
@@ -1,15 +1,15 @@
 package com.example.repeatrecyclerview.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.repeatrecyclerview.MainActivity
 import com.example.repeatrecyclerview.R
 import com.example.repeatrecyclerview.databinding.ListItemBinding
 import com.example.repeatrecyclerview.model.Messages
 
-class ItemAdapter(private val context: Context, private val dataset: List<Messages>) :
+class ItemAdapter(private val context: MainActivity, private val dataset: List<Messages>) :
     RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
     class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val binding = ListItemBinding.bind(view)
@@ -26,4 +26,6 @@ class ItemAdapter(private val context: Context, private val dataset: List<Messag
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = dataset[position]
         }
+
+
     }
